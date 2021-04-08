7ff7fb8bb472390a5440921c6496e1c36731b8da
nikeshchavan <nikeshchavan17@gmail.com>
2021-04-08 15:57:34 +0530

    added camera and changed size of the screen

--- js/Game.js
+++ js/Game.js
@@ -16,6 +16,7 @@ class Game {
   }
 
   async start(){
+
     if(gameState === 0){
       player = new Player();
       var playerCountRef = await database.ref('playerCount').once("value");
@@ -26,25 +27,37 @@ class Game {
       form = new Form()
       form.display();
     }
+    car1=createSprite(100,200);
+    car2=createSprite(300,200);
+    car3=createSprite(500,200);
+    car4=createSprite(700,200);
+    cars=[car1,car2,car3,car4];
+
   }
 
   play(){
     form.hide();
-    textSize(30);
-    text("Game Start", 120, 100)
+    
     Player.getPlayerInfo();
 
     if(allPlayers !== undefined){
-      var display_position = 130;
+      var index=0;
+      var x=0;
+      var y;
       for(var plr in allPlayers){
-        if (plr === "player" + player.index)
-          fill("red")
-        else
-          fill("black");
+        index=index+1;
+        x=x+200;
+        y=displayHeight-allPlayers[plr].distance;
+        cars[index-1].x=x;
+        cars[index-1].y=y;
+        if(index===player.index){
+          cars[index-1].shapeColor="red";
+        camera.position.x=displayWidth/2;
+        camera.position.y=cars[index-1].y;
+        
+        }
 
-        display_position+=20;
-        textSize(15);
-        text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
+        
       }
     }
 
@@ -52,5 +65,7 @@ class Game {
       player.distance +=50
       player.update();
     }
+    drawSprites();
+
   }
 }
